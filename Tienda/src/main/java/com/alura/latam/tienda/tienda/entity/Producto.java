642d2d20f7d76622e62b149e6d87b4e3826f9e82
LAPTOP-O3L7EUHH\Juan Diego <avilameza@gmail.com>
2023-08-27 15:40:51 -0500

    actualizando la entidad Producto

--- Tienda/src/main/java/com/alura/latam/tienda/tienda/entity/Producto.java
+++ Tienda/src/main/java/com/alura/latam/tienda/tienda/entity/Producto.java
@@ -1,14 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.alura.latam.tienda.tienda.entity;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.*;
 
@@ -23,9 +22,7 @@ import lombok.*;
 @ToString
 @Entity
 @Table(name = "tb_producto")
-public class Producto {
-  
-    
+public class Producto implements Serializable  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -33,5 +30,14 @@ public class Producto {
     private String nombre;
     private String descripcion;
     private BigDecimal precio;
-    
+    private LocalDate fechaRegistro = LocalDate.now();
+    @ManyToOne
+    private Categoria categoria;
+
+    public Producto(String nombre, String descripcion, BigDecimal precio, Categoria categoria) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
 }
